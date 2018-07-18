c7e646a3c78df5809ed2aac37e9f1d85fdf27cdc
Caca <eko.budi2283@gmail.com>
2018-07-18 21:56:25 +0700

    Edit Snappy PDF for Laravel Report

--- config/snappy.php
+++ config/snappy.php
@@ -5,7 +5,7 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltopdf',
+        'binary'  => '"C:\wkhtmltopdf\bin\wkhtmltopdf.exe"',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
